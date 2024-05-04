6e26d600be8dd48aaa74966751c076768e771452
CSK <cskcvarma13@gmail.com>
2024-05-04 10:12:48 -0400

    Zustand + React Query setup

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,11 +1,38 @@
 import React from 'react';
 import './App.css';
+import {useGitDetails} from "./store";
 
+
+export const Left= (): React.JSX.Element => {
+    const {token, addToken} = useGitDetails();
+
+
+    return (
+        <>
+            <div className="Left">{token}</div>
+            <button onClick={
+                () => addToken("New Tokens")
+            }>Click Me
+            </button>
+        </>
+
+    );
+}
+
+export const Right = (): React.JSX.Element => {
+    const {token} = useGitDetails();
+
+    return (
+        <div className="Right">{token}</div>
+    );
+}
 
 
 export const App = (): React.JSX.Element => {
     return (
-      <div className="App">Hello World</div>
+        <>
+            <Left/> | <Right/>
+      </>
   );
 }
 
