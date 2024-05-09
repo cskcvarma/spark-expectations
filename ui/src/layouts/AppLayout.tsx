05533b4676fd0b704c407d3c1e3bae1238a6a13f
CSK <cskcvarma13@gmail.com>
2024-05-09 13:54:00 -0400

    Add a sample NavBar

--- ui/src/layouts/AppLayout.tsx
+++ ui/src/layouts/AppLayout.tsx
@@ -2,11 +2,16 @@ import { AppShell } from '@mantine/core';
 import React from 'react';
 
 import { Header } from '@/components';
+import { NavBar } from '@/components/NavBar';
 
 export const AppLayout = () => (
   <AppShell header={{ height: 60 }} navbar={{ width: 300, breakpoint: 'sm' }} padding="md">
     {/*TODO: data-testid is not retained after the component is mounted. Need to investigate this*/}
     <Header data-testid="header" />
+    <AppShell.Navbar p="md" data-testid="navbar">
+      <NavBar />
+    </AppShell.Navbar>
+
     {/*<AppShell.Navbar p="md" data-testid="navbar">*/}
     {/*  Navbar*/}
     {/*  {Array(15)*/}
