5b4e91c30edfef2def06a1b9f6663cb8266e36d1
CSK <cskcvarma13@gmail.com>
2024-06-03 19:43:27 -0400

    rendering yaml files in each project

--- ui/src/api/repos/github/use-repo.ts
+++ ui/src/api/repos/github/use-repo.ts
@@ -17,8 +17,56 @@ export const getRepoFn = async (
   return response.data;
 };
 
-export const useRepo = (owner: string | undefined, repoName: string | undefined, path = '') =>
+export const getAllYamlFilesFn = async (
+  owner: string | undefined,
+  repoName: string | undefined,
+  initialPath = ''
+) => {
+  async function fetchDirectory(path: any) {
+    try {
+      const response = await apiClient.get(`repos/${owner}/${repoName}/contents/${path}`, {
+        headers: {
+          Authorization: `Bearer ${useAuthStore.getState().token}`,
+        },
+      });
+      return response.data;
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
+
+  const fetchRecursive = async (path: string) => {
+    const items = await fetchDirectory(path);
+    const files = items.filter(
+      (item: { type: string; name: string }) => item.type === 'file' && item.name.endsWith('.yaml')
+    );
+    const directories = items.filter((item: { type: string }) => item.type === 'dir');
+
+    const directoryPromises = directories.map((directory: { path: string }) =>
+      fetchRecursive(directory.path)
+    );
+    const results = await Promise.all(directoryPromises);
+    return files.concat(results.flat());
+  };
+
+  return fetchRecursive(initialPath);
+};
+
+export const useRepo = (
+  owner: string | undefined,
+  repoName: string | undefined,
+  path = '',
+  isEnabled = true
+) =>
   useQuery({
     queryKey: repoQueryKeys.detail([owner, repoName, path]),
-    queryFn: () => getRepoFn(owner, repoName, path),
+    queryFn: () => getAllYamlFilesFn(owner, repoName, path),
+    enabled: isEnabled,
   });
+
+// function filterYAMLFiles(data: any) {
+//   return data.filter(
+//     (item: any) => item.type === 'dir' || (item.type === 'file' && item.name.endsWith('.yaml'))
+//   );
+// }
--- ui/src/components/FilesList/FilesList.tsx
+++ ui/src/components/FilesList/FilesList.tsx
@@ -1,10 +1,17 @@
+import { List, Paper, ScrollArea, Text, Title, Tooltip } from '@mantine/core';
 import { useRepoStore } from '@/store';
 import { useRepo } from '@/api';
 import { Loading } from '@/components';
 
 export const FilesList = () => {
   const { selectedRepo } = useRepoStore();
-  const { data, isLoading, isError } = useRepo(selectedRepo?.owner?.login, selectedRepo?.name);
+
+  const { data, isLoading, isError } = useRepo(
+    selectedRepo?.owner?.login,
+    selectedRepo?.name,
+    '',
+    true
+  );
 
   if (isLoading) {
     return <Loading />;
@@ -15,17 +22,58 @@ export const FilesList = () => {
   }
 
   return (
-    <>
-      <div>{selectedRepo?.name}</div>
-      {data?.map((file: any) => {
-        if (file.name.endsWith('.txt')) {
-          return (
-            <div key={file.name}>
-              <a href={file}>{file.name}</a>
-            </div>
-          );
-        }
-      })}
-    </>
+    <Paper shadow="sm" radius="md" p="md" withBorder>
+      <Title order={3} style={{ marginBottom: 20 }}>
+        YAML Files
+      </Title>
+      <ScrollArea style={{ height: 300 }}>
+        <List spacing="sm" size="sm" center>
+          {data.map((file: any, index: number) => (
+            <List.Item key={index}>
+              <Tooltip label={file.path} position="bottom" withArrow>
+                <Text style={{ cursor: 'pointer' }}>{file.name}</Text>
+              </Tooltip>
+            </List.Item>
+          ))}
+        </List>
+      </ScrollArea>
+    </Paper>
   );
 };
+//
+// const NestedList = ({ item }) => {
+//   const { selectedRepo } = useRepoStore();
+//   const [isOpen, setIsOpen] = useState(false);
+//   const hasChildren = item.type === 'dir';
+//
+//   const { data, isLoading, isError } = useRepo(
+//     selectedRepo?.owner?.login,
+//     selectedRepo?.name,
+//     item.path,
+//     isOpen && hasChildren
+//   );
+//
+//   const toggle = () => setIsOpen(!isOpen);
+//
+//   return (
+//     <div>
+//       <Group position="apart" style={{ cursor: 'pointer', alignItems: 'center' }} onClick={toggle}>
+//         <Text>{item.name}</Text>
+//         {hasChildren && (
+//           <ThemeIcon size="sm">
+//             <IconChevronRight style={{ transform: isOpen ? 'rotate(90deg)' : 'rotate(0deg)' }} />
+//           </ThemeIcon>
+//         )}
+//       </Group>
+//       {isLoading && <Loader />}
+//       {isError && <Text color="red">Failed to load data</Text>}
+//       {isOpen && hasChildren && (
+//         <Collapse in={isOpen}>
+//           <Group direction="column" spacing="xs">
+//             {data?.map((child: any) => <NestedList key={child.sha} item={child} />)}
+//           </Group>
+//         </Collapse>
+//       )}
+//     </div>
+//   );
+// };
