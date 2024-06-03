5bc21f38951549f1301b6c5ce1d44f37fbee899e
CSK <cskcvarma13@gmail.com>
2024-06-03 10:15:12 -0400

    WIP

--- ui/src/components/Navigator/Navigator.tsx
+++ ui/src/components/Navigator/Navigator.tsx
@@ -1,23 +1,34 @@
 import React from 'react';
 
-import { AppShell, Container, Grid, Skeleton } from '@mantine/core';
+import { AppShell, Button, Container, Grid, Skeleton, Stack } from '@mantine/core';
 import { Header } from '@/components';
+import { useOrganizations } from '@/api/organizations';
 
 const child = <Skeleton height="900" radius="md" animate={false} />;
 
-export const Navigator = () => (
-  <AppShell>
-    <AppShell.Header>
-      <Header />
-    </AppShell.Header>
-    <AppShell.Main>
-      <Container fluid mt={50}>
-        <Grid>
-          <Grid.Col span={3}>{child}</Grid.Col>
-          <Grid.Col span={6}>{child}</Grid.Col>
-          <Grid.Col span={3}>{child}</Grid.Col>
-        </Grid>
-      </Container>
-    </AppShell.Main>
-  </AppShell>
-);
+export const Navigator = () => {
+  const { data } = useOrganizations();
+  console.log(data);
+  return (
+    <AppShell>
+      <AppShell.Header>
+        <Header />
+      </AppShell.Header>
+      <AppShell.Main>
+        <Container fluid mt={50}>
+          <Grid>
+            <Grid.Col span={2}>
+              <Stack gap="md">
+                <Button>1</Button>
+                <Button>1</Button>
+                <Button>1</Button>
+              </Stack>
+            </Grid.Col>
+            <Grid.Col span={8}>{child}</Grid.Col>
+            <Grid.Col span={2}>{child}</Grid.Col>
+          </Grid>
+        </Container>
+      </AppShell.Main>
+    </AppShell>
+  );
+};
